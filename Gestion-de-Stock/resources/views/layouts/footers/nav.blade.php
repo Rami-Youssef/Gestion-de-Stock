a3c0891e3edafb8c08c82a02ff372dfb3648dea3
Rami Youssef <98215523+Rami-Youssef@users.noreply.github.com>
2025-06-23 11:45:41 +0100

    fixed footer

--- Gestion-de-Stock/resources/views/layouts/footers/nav.blade.php
+++ Gestion-de-Stock/resources/views/layouts/footers/nav.blade.php
@@ -1,26 +1,22 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp;
-            <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Updivision</a>
+            &copy; {{ now()->year }} <span class="font-weight-bold ml-1">Gestion de Stock</span>
         </div>
     </div>
     <div class="col-xl-6">
         <ul class="nav nav-footer justify-content-center justify-content-xl-end">
             <li class="nav-item">
-                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+                <a href="{{ route('dashboard') }}" class="nav-link">Tableau de Bord</a>
             </li>
             <li class="nav-item">
-                <a href="https://www.updivision.com" class="nav-link" target="_blank">Updivision</a>
+                <a href="{{ route('categories.index') }}" class="nav-link">Catégories</a>
             </li>
             <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+                <a href="{{ route('produits.index') }}" class="nav-link">Produits</a>
             </li>
             <li class="nav-item">
-                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+                <a href="{{ route('mouvements.index') }}" class="nav-link">Mouvements</a>
             </li>
         </ul>
     </div>
